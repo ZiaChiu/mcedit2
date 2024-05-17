ce4ddd8f84e05f32a29cd254592d41b9a25ec57c
zia <chinese.zhaoziye@gmail.com>
2024-05-18 06:10:06 +0930

    Py2/PySide to Py3/PySide6
    Import Update:
    
    Changed the import from PySide to PySide6.
    Module Update:
    
    Updated the import from QtGui to QtWidgets, as QMessageBox is now part of QtWidgets in PySide6.
    Function Update:
    
    The function NotImplementedYet remains the same, but it uses QtWidgets.QMessageBox instead of QtGui.QMessageBox.

--- mcedit2/util/dialogs.py
+++ mcedit2/util/dialogs.py
@@ -1,11 +1,7 @@
-"""
-    dialogs
-"""
-from __future__ import absolute_import, division, print_function
-from PySide import QtGui
+from PySide6 import QtWidgets
 import logging
 
 log = logging.getLogger(__name__)
 
 def NotImplementedYet():
-    QtGui.QMessageBox.information(None, "Not Implemented Yet!", "Not Implemented Yet!")
+    QtWidgets.QMessageBox.information(None, "Not Implemented Yet!", "Not Implemented Yet!")
