925bcf3d9f55cce00672ed0ab6ed10a454557459
zia <chinese.zhaoziye@gmail.com>
2024-05-18 06:12:06 +0930

    Py2/PySide to Py3/PySide6
    Import Update:
    
    Changed the import to use PySide6.scripts.pyside_tool.pyside6_uic for compiling UI files.
    Module Update:
    
    The function compileUiDir from PySide is replaced with a custom compile_ui_dir function that uses pyside6_uic.compileUi to compile .ui files.
    Function Update:
    
    Added compile_ui_dir function to iterate through the directory and compile each .ui file it finds into a .py file using pyside6_uic.compileUi.
    Logging:
    
    Added logging to show which files are being compiled.

--- mcedit2/util/gen_ui.py
+++ mcedit2/util/gen_ui.py
@@ -1,12 +1,8 @@
-"""
-    gen_ui
-"""
-from __future__ import absolute_import, division, print_function, unicode_literals
 import logging
 import os
+from PySide6.scripts.pyside_tool import pyside6_uic
 
 log = logging.getLogger(__name__)
-
 
 def compile_ui():
     from mcedit2.util import resources
@@ -16,11 +12,21 @@ def compile_ui():
     src = resources.getSrcFolder()
     uiDir = os.path.join(src, "mcedit2", "ui")
 
-    from pysideuic import compileUiDir
-
     log.info("Compiling .ui files...")
-    compileUiDir(uiDir, recurse=True)
+    compile_ui_dir(uiDir, recurse=True)
     log.info("Done.")
+
+def compile_ui_dir(directory, recurse=False):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.ui'):
+                ui_file = os.path.join(root, file)
+                py_file = os.path.splitext(ui_file)[0] + ".py"
+                log.info(f"Compiling {ui_file} to {py_file}")
+                pyside6_uic.compileUi(ui_file, py_file)
+
+        if not recurse:
+            break
 
 if __name__ == '__main__':
     compile_ui()
