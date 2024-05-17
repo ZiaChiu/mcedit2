2f760c2660fbf6160ed58436e2b0047a03f06eb3
zia <chinese.zhaoziye@gmail.com>
2024-05-18 03:30:05 +0930

    Py2/PySide to Py3/PySide6

--- mcedit2/util/directories/posix.py
+++ mcedit2/util/directories/posix.py
@@ -15,18 +15,24 @@ log = logging.getLogger(__name__)
 # xxx for .whl or distro-specific distrib, put user files in ~/.mcedit2
 
 def getUserFilesDirectory():
+    """
+    Get the directory where user files are stored on POSIX systems (Linux, etc.).
+    The directory depends on whether the application is run from a source checkout
+    or a regular installation.
+    """
+
     # On Linux, the FS encoding is given by the current locale
     # Linux filenames are defined to be bytestrings.
 
     # We handle filenames internally as 'unicode', so decode 'sys.argv[0]'
-    # If a linux filename cannot be decoded with the current locale, ignore it.
+    # If a Linux filename cannot be decoded with the current locale, ignore it.
     # If this filename is the script filename, you lose.
 
     if resources.isSrcCheckout():
         # Source checkouts don't use the same folder as regular installs.
         dataDir = os.path.join(os.path.dirname(resources.getSrcFolder()), "MCEdit 2 Files")
     else:
-        dataDir = os.path.expanduser(u"~/.mcedit2")
+        dataDir = os.path.expanduser("~/.mcedit2")
 
     if not os.path.exists(dataDir):
         os.makedirs(dataDir)
