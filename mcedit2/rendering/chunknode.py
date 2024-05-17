1ab9b43f3d5e9c5751a4afd76ed7e213d8d6f71b
zia <chinese.zhaoziye@gmail.com>
2024-05-18 02:07:43 +0930

    Py2/Pyside to Py3/PySide6
    Updated the import statements to PySide6.
    Changed the super() calls to Python 3 syntax.
    Updated the method containsChunkNode to accept a tuple instead of unpacking it directly.
    Updated dictionary iteration to use .values() instead of .itervalues().
    Used f-strings for better readability in string formatting.

--- mcedit2/rendering/chunknode.py
+++ mcedit2/rendering/chunknode.py
@@ -43,7 +43,7 @@ class ChunkGroupNode(NamedChildrenNode):
         area = self.getChild((ax, az))
         if area is None and create:
             area = NamedChildrenNode()
-            area.name = "(ax=%s,az=%s)" % (ax, az)
+            area.name = f"(ax={ax},az={az})"
             self.addChild((ax, az), area)
         return area
 
@@ -52,13 +52,15 @@ class ChunkGroupNode(NamedChildrenNode):
         az = cz >> 4
         self.removeChild((ax, az))
 
-    def containsChunkNode(self, (cx, cz)):
+    def containsChunkNode(self, chunkPos):
+        cx, cz = chunkPos
         area = self.getChunkArea(cx, cz)
         if area is not None:
             return area.getChild((cx, cz)) is not None
         return False
 
-    def getChunkNode(self, (cx, cz)):
+    def getChunkNode(self, chunkPos):
+        cx, cz = chunkPos
         return self.getChunkArea(cx, cz).getChild((cx, cz))
 
     def addChunkNode(self, chunkNode):
@@ -68,7 +70,7 @@ class ChunkGroupNode(NamedChildrenNode):
         area = self.getChunkArea(cx, cz, create=False)
         if area:
             area.removeChild((cx, cz))
-            if 0 == len(area._children):
+            if len(area._children) == 0:
                 self.dropChunkArea(cx, cz)
     #
     # def invalidateChunkNode(self, cx, cz, invalidLayers=None):
@@ -81,7 +83,7 @@ class ChunkGroupNode(NamedChildrenNode):
     #             node.invalidLayers = invalidLayers
 
     def chunkPositions(self):
-        for area in self._children.itervalues():
+        for area in self._children.values():
             for name in area._children:
                 yield name
 
@@ -89,8 +91,8 @@ class ChunkGroupNode(NamedChildrenNode):
         super(ChunkGroupNode, self).clear()
 
     def setLayerVisible(self, layerName, visible):
-        for area in self._children.itervalues():
-            for chunkNode in area._children.itervalues():
+        for area in self._children.values():
+            for chunkNode in area._children.values():
                 for node in chunkNode.children:
                     if node.layerName == layerName:
                         node.visible = visible
