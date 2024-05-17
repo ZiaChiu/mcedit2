17da2f07c650ae70eede29fe5d5a4cbc88a12306
zia <chinese.zhaoziye@gmail.com>
2024-05-18 02:31:12 +0930

    Py2/Pyside to Py3/PySide6
    Updated import statements and usage for numpy to np.
    Used Python 3.x syntax for super() calls.
    Replaced .iteritems() with .items() for dictionary iteration.

--- mcedit2/rendering/loadablechunks.py
+++ mcedit2/rendering/loadablechunks.py
@@ -1,11 +1,11 @@
 """
-    ${NAME}
+${NAME}
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
 from collections import defaultdict
 import logging
 
-import numpy
+import numpy as np
 from OpenGL import GL
 
 from mcedit2.rendering.scenegraph import scenenode, rendernode
@@ -18,7 +18,7 @@ log.info("Making checkerboard texture...")
 color0 = (0xff, 0xff, 0xff, 0x22)
 color1 = (0xff, 0xff, 0xff, 0x44)
 
-floorTexImage = numpy.array([color0, color1, color1, color0], dtype='uint8')
+floorTexImage = np.array([color0, color1, color1, color0], dtype='uint8')
 
 class LoadableChunksRenderNode(rendernode.RenderNode):
     floorTexture = None
@@ -27,10 +27,9 @@ class LoadableChunksRenderNode(rendernode.RenderNode):
         if self.floorTexture is None:
             self.floorTexture = Texture(image=floorTexImage, width=2, height=2,
                                         minFilter=GL.GL_NEAREST,
-                                        magFilter=GL.GL_NEAREST,
-                                        )
+                                        magFilter=GL.GL_NEAREST)
             self.floorTexture.load()
-        super(LoadableChunksRenderNode, self).compile()
+        super().compile()
 
     def drawSelf(self):
         with gl.glPushAttrib(GL.GL_FOG_BIT | GL.GL_ENABLE_BIT):
@@ -49,7 +48,6 @@ class LoadableChunksRenderNode(rendernode.RenderNode):
 
             for vertexArray in self.sceneNode.createVertexArrays():
                 GL.glVertexPointer(3, GL.GL_FLOAT, 0, vertexArray.ravel())
-                # chunkPositions *= 8
                 GL.glTexCoordPointer(2, GL.GL_FLOAT, 0, (vertexArray[..., (0, 2)] / 32).ravel())
                 GL.glDrawArrays(GL.GL_QUADS, 0, len(vertexArray) * 4)
             GL.glDisableClientState(GL.GL_TEXTURE_COORD_ARRAY)
@@ -60,35 +58,24 @@ class LoadableChunksNode(scenenode.Node):
     RenderNodeClass = LoadableChunksRenderNode
 
     def __init__(self, dimension):
-        super(LoadableChunksNode, self).__init__()
+        super().__init__()
         self.dimension = dimension
 
-        # if self.skipLargeLevels: # and hasattr(self.dimension.worldEditor, 'worldFolder'):
-        #     try:
-        #         p = self.dimension.worldEditor.adapter.selectedRevision.getFolderPath('region')
-        #         if len(os.listdir(p)) > 50:  # 50 * 1024 chunks
-        #             return
-        #
-        #     except AttributeError:
-        #         log.exception("Don't know how to count region files in %s", self.dimension)
-        #         raise
-
     def createVertexArrays(self):
-
         if self.dimension.chunkCount:
             chunkSet = set(self.dimension.chunkPositions())
             sizedChunks = chunkMarkers(chunkSet)
 
             def arrays():
-                for size, chunks in sizedChunks.iteritems():
+                for size, chunks in sizedChunks.items():
                     if not len(chunks):
                         continue
-                    chunks = numpy.array(chunks, dtype='float32')
+                    chunks = np.array(chunks, dtype='float32')
 
-                    chunkPositions = numpy.zeros(shape=(chunks.shape[0], 4, 3), dtype='float32')
-                    chunkPositions[:, :, (0, 2)] = numpy.array(((0, 0), (0, 1), (1, 1), (1, 0)), dtype='float32')
+                    chunkPositions = np.zeros(shape=(chunks.shape[0], 4, 3), dtype='float32')
+                    chunkPositions[:, :, (0, 2)] = np.array(((0, 0), (0, 1), (1, 1), (1, 0)), dtype='float32')
                     chunkPositions[:, :, (0, 2)] *= size
-                    chunkPositions[:, :, (0, 2)] += chunks[:, numpy.newaxis, :]
+                    chunkPositions[:, :, (0, 2)] += chunks[:, np.newaxis, :]
                     chunkPositions *= 16
                     yield chunkPositions
 
@@ -107,7 +94,6 @@ def chunkMarkers(chunkSet):
         cz &= ~size
         return [(cx, cz), (cx + size, cz), (cx + size, cz + size), (cx, cz + size)]
 
-    # lastsize = 6
     size = 1
     while True:
         nextsize = size << 1
