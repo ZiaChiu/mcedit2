7b475f447d499ba5cbd3a7acf9ea0d053b9de436
zia <chinese.zhaoziye@gmail.com>
2024-05-17 22:51:34 +0930

    Py2/PySide to Py3/PySide6

--- mcedit2/panels/pending_imports.py
+++ mcedit2/panels/pending_imports.py
@@ -2,16 +2,16 @@
     pending_imports
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
-from PySide import QtGui
+from PySide6 import QtWidgets, QtCore
 import logging
 
 log = logging.getLogger(__name__)
 
-class PendingImportsWidget(QtGui.QWidget):
+class PendingImportsWidget(QtWidgets.QWidget):
     def __init__(self):
         super(PendingImportsWidget, self).__init__()
-        self.importsListWidget = QtGui.QListView()
-        self.importsListModel = QtGui.QStandardItemModel()
+        self.importsListWidget = QtWidgets.QListView()
+        self.importsListModel = QtCore.QStandardItemModel()
         self.importsListWidget.setModel(self.importsListModel)
         self.importsListWidget.clicked.connect(self.listClicked)
         self.importsListWidget.doubleClicked.connect(self.listDoubleClicked)
