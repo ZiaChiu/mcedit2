14b31e58b05a80bbf7439bba01e329a6720550af
zia <chinese.zhaoziye@gmail.com>
2024-05-18 18:06:20 +0930

    Py2/PySide to Py3/PySide6
    Imports:
    
    Updated imports for PySide6.
    Changed from PySide import QtGui to from PySide6 import QtGui, QtCore.

--- mcedit2/util/mceaction.py
+++ mcedit2/util/mceaction.py
@@ -3,19 +3,16 @@
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
 import logging
-from PySide import QtGui
+from PySide6 import QtGui, QtCore
 from mcedit2.util.settings import Settings
 
 log = logging.getLogger(__name__)
 
-
 _binding_names = {}
-
 
 def ChangeBinding(shortcutName, shortcutString):
     settings = Settings()
     settings.setValue("keybinding/" + shortcutName, shortcutString)
-
 
 class MCEAction(QtGui.QAction):
     def __init__(self, name, parent, shortcutName=None, *args, **kwargs):
